238e8d4fcd568e4abbb21e81e00ef50c56e1cafe
Sajan <work@sajankumarv.com>
2025-02-22 18:47:32 +0100

    Folder structure fixed (#7)

---
+++ src/FastModule.EntityFrameworkCore/Module.cs
@@ -0,0 +1,16 @@
+﻿using FastModule.EntityFrameworkCore.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace FastModule.EntityFrameworkCore;
+
+public sealed class Module : Core.FastModule
+{
+    public override void Register(IServiceCollection services, Action<DbContextOptionsBuilder>? options = null)
+    {
+        if (options is null) return;
+        services.AddDbContext<AppDbContext>(options);
+    }
+}
